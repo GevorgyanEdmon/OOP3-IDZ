c4115230c3b83bc40eaa5fd93d3df0288197d82c
yaolg <yaolg@DESKTOP-PS4EPC5>
2022-10-23 17:02:33 +0300

    Добавьте файлы проекта.

---
+++ OOP3/Program.cs
@@ -0,0 +1,14 @@
+﻿using OOP3_IDZ_LIB;
+namespace ЛР3_ИДЗ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matrix matrix = new Matrix(3, 3);
+            Matrix.PrintMatrix(matrix);
+            Matrix.GetMinMax(matrix);
+            Matrix.PrintResultMatrix(matrix);
+        }
+    }
+}
